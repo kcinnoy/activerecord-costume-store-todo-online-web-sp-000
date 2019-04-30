b703999a9d005891119951b122d09b519983c193
Yonnick <yonnick00@gmail.com>
2019-04-30 21:23:12 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,2 +1,12 @@
 class CostumeStore  < ActiveRecord::Base
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_employees
+      t.boolean :still_in_business
+      t.timestamps
+    end
+  end
 end
