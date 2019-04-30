3edf6c52aaad83be15588f2f2d2135a2414ce660
Yonnick <yonnick00@gmail.com>
2019-04-30 21:17:39 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,11 @@
 class Costume  < ActiveRecord::Base
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.string :image_url
+      
+    end
+  end
 end
