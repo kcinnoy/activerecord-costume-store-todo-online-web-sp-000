7a34d27700e4fd234ad4fcdbccd1bac404de52ed
Yonnick <yonnick00@gmail.com>
2019-04-30 21:38:23 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,13 +1,2 @@
-class CreateCostumeStores < ActiveRecord::Migration[5.1]
-  def change
-    create_table :costume_stores do |t|
-      t.text :name
-      t.text :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
-    end
-  end
+class CostumeStore < ActiveRecord::Base
 end
