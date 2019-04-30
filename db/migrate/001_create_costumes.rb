40243059942046728a705cac98190ebe710fb619
Yonnick <yonnick00@gmail.com>
2019-04-30 21:37:00 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_costumes.rb
@@ -0,0 +1,11 @@
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.text :name
+      t.integer :price
+      t.text :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
